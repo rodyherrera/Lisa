b4e82a01c73ea2255b196ba81ae117ca787f80c1
rodyherrera <contact@rodyherrera.com>
2023-11-05 18:54:21 -0300

    HTTP Request

--- Source/Utilities/HTTPServer/HTTPServer.cpp
+++ Source/Utilities/HTTPServer/HTTPServer.cpp
@@ -30,6 +30,47 @@ JSObjectRef HTTPServer::CreateRequestObject(JSContextRef Context, httplib::Reque
         JSCWrapper::SetStringProperty(Context, QueryParamsObject, QueryParam.first.c_str(), QueryParam.second.c_str());
     JSObjectSetProperty(Context, RequestObject, JSStringCreateWithUTF8CString("Query"), QueryParamsObject, kJSPropertyAttributeNone, NULL);
 
+    JSCWrapper::CreateFunction(Context, RequestObject, "GetFileValue", [](JSContextRef Context, JSObjectRef Function, JSObjectRef ThisObject, size_t ArgumentsLength, const JSValueRef Arguments[], JSValueRef* Exception) -> JSValueRef {
+        httplib::Request& Request = *(httplib::Request*)JSCWrapper::GetObjectFromObject(Context, ThisObject, "Request");
+        const std::string FileName = JSCWrapper::GetStringFromJSValue(Context, Arguments[0]);
+        httplib::MultipartFormData FormData = Request.get_file_value(FileName);
+        JSObjectRef FileObject = JSObjectMake(Context, NULL, NULL);
+        JSCWrapper::SetStringProperty(Context, FileObject, "Name", FormData.filename.c_str());
+        JSCWrapper::SetStringProperty(Context, FileObject, "ContentType", FormData.content_type.c_str());
+        JSCWrapper::SetStringProperty(Context, FileObject, "Data", FormData.content.c_str());
+        return FileObject;
+    });
+
+    JSCWrapper::CreateFunction(Context, RequestObject, "IsMultipartFormData", [](JSContextRef Context, JSObjectRef Function, JSObjectRef ThisObject, size_t ArgumentsLength, const JSValueRef Arguments[], JSValueRef* Exception) -> JSValueRef {
+        httplib::Request& Request = *(httplib::Request*)JSCWrapper::GetObjectFromObject(Context, ThisObject, "Request");
+        return JSValueMakeBoolean(Context, Request.is_multipart_form_data());
+    });
+
+    JSCWrapper::CreateFunction(Context, RequestObject, "GetFileValues", [](JSContextRef Context, JSObjectRef Function, JSObjectRef ThisObject, size_t ArgumentsLength, const JSValueRef Arguments[], JSValueRef* Exception) -> JSValueRef {
+        httplib::Request& Request = *(httplib::Request*)JSCWrapper::GetObjectFromObject(Context, ThisObject, "Request");
+        const std::string FileName = JSCWrapper::GetStringFromJSValue(Context, Arguments[0]);
+        std::vector<httplib::MultipartFormData> FormDatas = Request.get_file_values(FileName);
+        JSObjectRef FileValuesObject = JSObjectMake(Context, NULL, NULL);
+        for(size_t FileIterator = 0; FileIterator < FormDatas.size(); FileIterator++){
+            JSObjectRef FileObject = JSObjectMake(Context, NULL, NULL);
+            JSCWrapper::SetStringProperty(Context, FileObject, "Name", FormDatas[FileIterator].filename.c_str());
+            JSCWrapper::SetStringProperty(Context, FileObject, "ContentType", FormDatas[FileIterator].content_type.c_str());
+            JSCWrapper::SetStringProperty(Context, FileObject, "Data", FormDatas[FileIterator].content.c_str());
+            JSCWrapper::SetObjectInObject(Context, FileValuesObject, std::to_string(FileIterator).c_str(), FileObject);
+        }
+        return FileValuesObject;
+    });
+
+    JSObjectRef FilesObject = JSObjectMake(Context, NULL, NULL);
+    for(auto File : Request->files){
+        JSObjectRef FileObject = JSObjectMake(Context, NULL, NULL);
+        JSCWrapper::SetStringProperty(Context, FileObject, "Name", File.first.c_str());
+        JSCWrapper::SetStringProperty(Context, FileObject, "ContentType", File.second.content_type.c_str());
+        JSCWrapper::SetStringProperty(Context, FileObject, "Data", File.second.content.c_str());
+        JSCWrapper::SetObjectInObject(Context, FilesObject, File.first.c_str(), FileObject);
+    }
+    JSObjectSetProperty(Context, RequestObject, JSStringCreateWithUTF8CString("Files"), FilesObject, kJSPropertyAttributeNone, NULL);
+
     JSCWrapper::SetStringProperty(Context, RequestObject, "Path", Request->path.c_str());
     JSCWrapper::SetStringProperty(Context, RequestObject, "Method", Request->method.c_str());
     JSCWrapper::SetStringProperty(Context, RequestObject, "Body", Request->body.c_str());
@@ -37,6 +78,12 @@ JSObjectRef HTTPServer::CreateRequestObject(JSContextRef Context, httplib::Reque
     JSCWrapper::SetStringProperty(Context, RequestObject, "Target", Request->target.c_str());
     JSCWrapper::SetIntegerProperty(Context, RequestObject, "RemotePort", Request->remote_port);
     JSCWrapper::SetStringProperty(Context, RequestObject, "RemoteAddress", Request->remote_addr.c_str());
+
+    std::vector<std::string> Matches;
+    for(auto Match : Request->matches)
+        Matches.push_back(Match);
+    JSObjectRef MatchesObject = JSObjectMakeArray(Context, Matches.size(), NULL, NULL);
+    JSObjectSetProperty(Context, RequestObject, JSStringCreateWithUTF8CString("Matches"), MatchesObject, kJSPropertyAttributeNone, NULL);
 
     JSCWrapper::CreateFunction(Context, RequestObject, "GetHeaderValue", [](JSContextRef Context, JSObjectRef Function, JSObjectRef ThisObject, size_t ArgumentsLength, const JSValueRef Arguments[], JSValueRef* Exception) -> JSValueRef {
         httplib::Request& Request = *(httplib::Request*)JSCWrapper::GetObjectFromObject(Context, ThisObject, "Request");
@@ -94,21 +141,21 @@ JSObjectRef HTTPServer::CreateResponseObject(JSContextRef Context, httplib::Resp
     JSObjectRef ResponseObject = JSObjectMake(Context, NULL, NULL);
     JSCWrapper::SetObjectInObject(Context, ResponseObject, "Response", Response);
 
-    JSCWrapper::CreateFunction(Context, ResponseObject, "GetHeaderValue", [](JSContextRef Context, JSObjectRef Function, JSObjectRef ThisObject, size_t ArgumentsLength, const JSValueRef Arguments[], JSValueRef* Exception){
+    JW_CREATE_FUNC(GetHeaderValue, ResponseObject){
         httplib::Response& Response = *(httplib::Response*)JSCWrapper::GetObjectFromObject(Context, ThisObject, "Response");
         const std::string HeaderName = JSCWrapper::GetStringFromJSValue(Context, Arguments[0]);
         const std::string HeaderValue = Response.get_header_value(HeaderName.c_str());
         return JSCWrapper::CreateString(Context, HeaderValue.c_str());
     });
     
-    JSCWrapper::CreateFunction(Context, ResponseObject, "GetHeaderValueCount", [](JSContextRef Context, JSObjectRef Function, JSObjectRef ThisObject, size_t ArgumentsLength, const JSValueRef Arguments[], JSValueRef* Exception){
+    JW_CREATE_FUNC(GetHeaderValueCount, ResponseObject){
         httplib::Response& Response = *(httplib::Response*)JSCWrapper::GetObjectFromObject(Context, ThisObject, "Response");
         const std::string HeaderName = JSCWrapper::GetStringFromJSValue(Context, Arguments[0]);
         const int HeaderValueCount = Response.get_header_value_count(HeaderName.c_str());
         return JSValueMakeNumber(Context, HeaderValueCount);
     });
 
-    JSCWrapper::CreateFunction(Context, ResponseObject, "HasHeader", [](JSContextRef Context, JSObjectRef Function, JSObjectRef ThisObject, size_t ArgumentsLength, const JSValueRef Arguments[], JSValueRef* Exception){
+    JW_CREATE_FUNC(HasHeader, ResponseObject){
         httplib::Response& Response = *(httplib::Response*)JSCWrapper::GetObjectFromObject(Context, ThisObject, "Response");
         const std::string HeaderName = JSCWrapper::GetStringFromJSValue(Context, Arguments[0]);
         const bool HasHeader = Response.has_header(HeaderName.c_str());
@@ -126,7 +173,7 @@ JSObjectRef HTTPServer::CreateResponseObject(JSContextRef Context, httplib::Resp
     JSCWrapper::SetIntegerProperty(Context, ResponseObject, "Status", Response->status);
     JSCWrapper::SetStringProperty(Context, ResponseObject, "Reason", Response->reason.c_str());
 
-    JSCWrapper::CreateFunction(Context, ResponseObject, "SetHeader", [](JSContextRef Context, JSObjectRef Function, JSObjectRef ThisObject, size_t ArgumentsLength, const JSValueRef Arguments[], JSValueRef* Exception){
+    JW_CREATE_FUNC(SetHeader, ResponseObject){
         httplib::Response& Response = *(httplib::Response*)JSCWrapper::GetObjectFromObject(Context, ThisObject, "Response");
         const std::string HeaderName = JSCWrapper::GetStringFromJSValue(Context, Arguments[0]);
         const std::string HeaderValue = JSCWrapper::GetStringFromJSValue(Context, Arguments[1]);
@@ -134,21 +181,21 @@ JSObjectRef HTTPServer::CreateResponseObject(JSContextRef Context, httplib::Resp
         return JSC_MAKE_UNDEFINED;
     });
     
-    JSCWrapper::CreateFunction(Context, ResponseObject, "SetRedirect", [](JSContextRef Context, JSObjectRef Function, JSObjectRef ThisObject, size_t ArgumentsLength, const JSValueRef Arguments[], JSValueRef* Exception){
+    JW_CREATE_FUNC(SetRedirect, ResponseObject){
         httplib::Response& Response = *(httplib::Response*)JSCWrapper::GetObjectFromObject(Context, ThisObject, "Response");
         const std::string Path = JSCWrapper::GetStringFromJSValue(Context, Arguments[0]);
         Response.set_redirect(Path.c_str());
         return JSC_MAKE_UNDEFINED;
     });
 
-    JSCWrapper::CreateFunction(Context, ResponseObject, "SetStatus", [](JSContextRef Context, JSObjectRef Function, JSObjectRef ThisObject, size_t ArgumentsLength, const JSValueRef Arguments[], JSValueRef* Exception){
+    JW_CREATE_FUNC(SetStatus, ResponseObject){
         httplib::Response& Response = *(httplib::Response*)JSCWrapper::GetObjectFromObject(Context, ThisObject, "Response");
         const int Status = JSValueToNumber(Context, Arguments[0], NULL);
         Response.status = Status;
         return JSC_MAKE_UNDEFINED;
     });
 
-    JSCWrapper::CreateFunction(Context, ResponseObject, "Send", [](JSContextRef Context, JSObjectRef Function, JSObjectRef ThisObject, size_t ArgumentsLength, const JSValueRef Arguments[], JSValueRef* Exception) -> JSValueRef {
+    JW_CREATE_FUNC(Send, ResponseObject){
         httplib::Response& Response = *(httplib::Response*)JSCWrapper::GetObjectFromObject(Context, ThisObject, "Response");
         const std::string ResponseString = JSCWrapper::GetStringFromJSValue(Context, Arguments[0]);
         const std::string ContentType = JSCWrapper::GetStringFromJSValue(Context, Arguments[1]);
